92bfd14311e4b5b765ced6a410e5f8d456063359
Vijay <vijayganganapalli@gmail.com>
2019-02-26 14:04:20 +0530

    change HomeScreen as MessageList

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vchat/message_list.dart';
 
 void main() => runApp(VchatApp());
 
@@ -10,74 +11,10 @@ class VchatApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
-      home: HomeScreen(
+      home: MessageList(
         title: 'Vchat',
       ),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  final String title;
-  final messages = const [
-    {
-      'subject': 'My first message',
-      'body':
-          'This is another message and is very very long so you can see and read the message. This is another message and is very very long so you can see this message.'
-    },
-    {
-      'subject': 'My second message',
-      'body':
-          'This is another message and is very very long so you can see and read the message.'
-    },
-    {
-      'subject': 'You won lottery',
-      'body':
-          'This is another message and is very very long so you can see and read the message.'
-    },
-    {
-      'subject': 'You should read this',
-      'body':
-          'This is another message and is very very long so you can see and read the message.'
-    },
-    {
-      'subject': 'You won lottery again',
-      'body':
-          'This is another message and is very very long so you can see and read the message.'
-    },
-    {
-      'subject': 'You should see this message',
-      'body': 'This is another message from your friend.'
-    }
-  ];
-
-  HomeScreen({Key key, this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: ListView.separated(
-        itemCount: messages.length,
-        separatorBuilder: (BuildContext context, int index) => Divider(),
-        itemBuilder: (BuildContext context, int index) {
-          var message = messages[index];
-          return ListTile(
-            isThreeLine: true,
-            leading: CircleAvatar(
-              child: Text('VK'),
-            ),
-            title: Text(message['subject']),
-            subtitle: Text(
-              message['body'],
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
